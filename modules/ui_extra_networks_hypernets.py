792fca5b010d8a9cb0e60f4436787859a793312c
SLAPaper <slapaper.pku@gmail.com>
2023-06-29 01:32:27 +0800

    fix: handle file change outside

--- modules/ui_extra_networks_hypernets.py
+++ modules/ui_extra_networks_hypernets.py
@@ -15,17 +15,20 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
         for index, (name, path) in enumerate(list(shared.hypernetworks.items())):
             path, ext = os.path.splitext(path)
 
-            yield {
-                "name": name,
-                "filename": path,
-                "preview": self.find_preview(path),
-                "description": self.find_description(path),
-                "search_term": self.search_terms_from_path(path),
-                "prompt": json.dumps(f"<hypernet:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
-                "local_preview": f"{path}.preview.{shared.opts.samples_format}",
-                "sort_keys": {'default': index, **self.get_sort_keys(path + ext)},
+            try:
+                yield {
+                    "name": name,
+                    "filename": path,
+                    "preview": self.find_preview(path),
+                    "description": self.find_description(path),
+                    "search_term": self.search_terms_from_path(path),
+                    "prompt": json.dumps(f"<hypernet:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
+                    "local_preview": f"{path}.preview.{shared.opts.samples_format}",
+                    "sort_keys": {'default': index, **self.get_sort_keys(path + ext)},
 
-            }
+                }
+            except:
+                continue
 
     def allowed_directories_for_previews(self):
         return [shared.cmd_opts.hypernetwork_dir]
--- modules/ui_extra_networks_textual_inversion.py
+++ modules/ui_extra_networks_textual_inversion.py
@@ -15,17 +15,20 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
     def list_items(self):
         for index, embedding in enumerate(list(sd_hijack.model_hijack.embedding_db.word_embeddings.values())):
             path, ext = os.path.splitext(embedding.filename)
-            yield {
-                "name": embedding.name,
-                "filename": embedding.filename,
-                "preview": self.find_preview(path),
-                "description": self.find_description(path),
-                "search_term": self.search_terms_from_path(embedding.filename),
-                "prompt": json.dumps(embedding.name),
-                "local_preview": f"{path}.preview.{shared.opts.samples_format}",
-                "sort_keys": {'default': index, **self.get_sort_keys(embedding.filename)},
+            try:
+                yield {
+                    "name": embedding.name,
+                    "filename": embedding.filename,
+                    "preview": self.find_preview(path),
+                    "description": self.find_description(path),
+                    "search_term": self.search_terms_from_path(embedding.filename),
+                    "prompt": json.dumps(embedding.name),
+                    "local_preview": f"{path}.preview.{shared.opts.samples_format}",
+                    "sort_keys": {'default': index, **self.get_sort_keys(embedding.filename)},
 
-            }
+                }
+            except:
+                continue
 
     def allowed_directories_for_previews(self):
         return list(sd_hijack.model_hijack.embedding_db.embedding_dirs)
