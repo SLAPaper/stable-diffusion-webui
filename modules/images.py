42cb920d64973fb075b1a452959486da11c2cf4f
SLAPaper <slapaper.pku@gmail.com>
2023-07-02 14:20:31 +0800

    info: fix info and comment

--- modules/images.py
+++ modules/images.py
@@ -633,14 +633,14 @@ def save_image(image, path, basename, seed=None, prompt=None, extension='png', i
         params.filename = fullfn_without_extension + extension
         fullfn = params.filename
 
-    # webp only allows for 16383*16383, for larger images, scaling down before safe, and also safe a jpeg file at original size
+    # webp only allows for 16383*16383, for larger images, scaling down before save, and also save a png file at original size
     # https://developers.google.com/speed/webp/faq#what_is_the_maximum_size_a_webp_image_can_be
     if extension.lower() == ".webp" and image.width > 16383 or image.height > 16383:
         import sys
         print(
             "[INFO]:", "webp has size limit of (16383, 16383),",
             f"while current size is ({image.width}, {image.height}),",
-            "will save a downscaled version in webp but also a jpg in original size",
+            "will save a downscaled version in webp but also a png in original size",
             file=sys.stderr
         )
 
