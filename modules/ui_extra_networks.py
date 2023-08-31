cf1763fc78a54199a522ce574f93792350d2c7ab
SLAPaper <slapaper.pku@gmail.com>
2023-08-31 16:45:29 +0800

    Squashed commit of the following:
    commit c2b1206aa40a702bbfe98ebda5ccd9aea3b2b957
    Merge: 189229bb ceb89722
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Aug 24 18:44:28 2023 +0800
    
        Merge branch 'v152' into v160RC
    
    commit ceb8972263c6b7258a6076f16089f4ac071a71a6
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Aug 24 00:09:42 2023 +0800
    
        fix: better handling webp PARTITION0_OVERFLOW
    
    commit ab78a8a851e49707230e64652812210530716f2d
    Merge: c9c8485b 7560aad1
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Aug 23 23:02:42 2023 +0800
    
        Merge branch 'v151' into v152
    
    commit 7560aad11182e7973deb54fac08280b09211b9f3
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Aug 23 10:10:51 2023 +0800
    
        fix: try to handle webp encoding error
    
    commit 3fc3711b99fc3ecd7fd8eea8b5251000ed312e0f
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Sat Aug 19 17:35:57 2023 +0800
    
        fix: improve webp size limit handling
    
    commit 870d65eab16829227cde073caf4e2734dcf67284
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Fri Aug 11 09:32:06 2023 +0800
    
        feat: error tolerence refresh
    
    commit d6dafd14e21f609ba607f439ff273688d352bf36
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Aug 10 07:55:39 2023 +0800
    
        feat: set default font to xyz plot
    
    commit ee09f273f1710ce9eb1f8d518e142ae512e319b5
    Merge: 1b9ac69a 56236dfd
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Jul 27 22:40:22 2023 +0800
    
        Merge remote-tracking branch 'origin/release_candidate' into rc
    
    commit 1b9ac69abcd6718c7c42f14e1a49a767ee72a2f8
    Merge: 8d481b01 3039925b
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Jul 27 02:31:23 2023 +0800
    
        Merge remote-tracking branch 'origin/release_candidate' into rc
    
    commit 8d481b01761bff08945f7aa04e620a2b7d1ee820
    Merge: 99ef3b6c 72331fba
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Jul 26 00:17:46 2023 +0800
    
        Merge branch 'rc' into v151RC
    
    commit 72331fbae4614f2b5dbb0677852c789756963639
    Merge: 2c11e900 e1c1c7e9
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Mon Jul 24 17:39:24 2023 +0800
    
        Merge branch 'v150rc' into rc
    
    commit e1c1c7e92955498ce0d07b1b8468d2a81c26b733
    Merge: eb7c9b58 42cb920d
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Jul 19 00:12:55 2023 +0800
    
        Merge branch 'v140' into v150RC
    
    commit 42cb920d64973fb075b1a452959486da11c2cf4f
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Sun Jul 2 14:20:31 2023 +0800
    
        info: fix info and comment
    
    commit c90907b64cdb6a001ca0b49d3eedd473503a3466
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Sun Jul 2 00:31:04 2023 +0800
    
        fix: avoid jpeg size limit
    
    commit d06fc206a996362acc49e939935134f8fda50a0f
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Jun 29 23:58:09 2023 +0800
    
        lint: make linter happy
    
    commit 792fca5b010d8a9cb0e60f4436787859a793312c
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Thu Jun 29 01:32:27 2023 +0800
    
        fix: handle file change outside
    
    commit aa025348ed66d9e98dc0c631077c3e0df5aa7f46
    Merge: 394ffa7b bf31e5bb
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Jun 28 00:53:06 2023 +0800
    
        Merge branch 'v140RC' into v140
    
    commit bf31e5bb8788079a3afe63dc4237e8d04ea4a82b
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Wed Jun 28 00:50:42 2023 +0800
    
        fix: webp size limit
    
    commit 954b60d7d63cdb7bc59b7afa0709e729416b9200
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Tue Jun 27 11:41:31 2023 +0800
    
        feat: handle preview image priority properly
    
    commit a026b74459404cd3c3369bd4c50b9fec6663a403
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Mon Jun 26 00:58:02 2023 +0800
    
        fix: frozen model list before iteration
    
        avoid "RuntimeError: dictionary changed size during iteration"
    
    commit 8196667269b6d23c72d6059a9b12c8a999d5e2c3
    Author: SLAPaper <slapaper.pku@gmail.com>
    Date:   Tue May 2 05:04:41 2023 +0800
    
        feat: add alt karras sampler

--- modules/ui_extra_networks.py
+++ modules/ui_extra_networks.py
@@ -277,7 +277,10 @@ class ExtraNetworksPage:
         if shared.opts.samples_format not in preview_extensions:
             preview_extensions.append(shared.opts.samples_format)
 
-        potential_files = sum([[path + "." + ext, path + ".preview." + ext] for ext in preview_extensions], [])
+        from itertools import chain
+        potential_files = chain(
+            (path + "." + ext for ext in preview_extensions),
+            (path + ".preview." + ext for ext in preview_extensions))
 
         for file in potential_files:
             if os.path.isfile(file):
@@ -394,8 +397,14 @@ def create_ui(interface: gr.Blocks, unrelated_tabs, tabname):
         return ui.pages_contents
 
     def refresh():
+        import sys
+        import traceback
         for pg in ui.stored_extra_pages:
-            pg.refresh()
+            try:
+                pg.refresh()
+            except Exception:
+                traceback.print_exc(file=sys.stderr)
+                continue
 
         ui.pages_contents = [pg.create_html(ui.tabname) for pg in ui.stored_extra_pages]
 
